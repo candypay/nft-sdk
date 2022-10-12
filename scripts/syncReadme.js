ca733f681455ef8f5f645220c5d13a7477dd29aa
Mukesh <hey.mukesh272921@gmail.com>
2022-10-12 21:28:24 +0530

    🧹 chore: delete scripts folder

--- scripts/syncReadme.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-const rootReadmeContents = fs.readFileSync("README.md");
-const packageReadmeContents = fs.readFileSync("package/README.md");
-
-if (rootReadmeContents !== packageReadmeContents) {
-  fs.writeFileSync("package/README.md", rootReadmeContents, "utf-8");
-}
