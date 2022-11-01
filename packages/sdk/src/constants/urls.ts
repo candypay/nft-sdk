7600cd376c3760963525f71d45833bc057456c5c
Juan Ruiz de Bustillo <juan@decaf.so>
2022-11-01 18:24:51 +0000

    mainnet-beta URL is wrong

--- packages/sdk/src/constants/urls.ts
+++ packages/sdk/src/constants/urls.ts
@@ -1,7 +1,7 @@
 const rpc = new Map([
   ["devnet", "https://api.devnet.solana.com"],
   ["testnet", "https://api.testnet.solana.com"],
-  ["mainnet-beta", "https://mainnet-beta.solana.com"],
+  ["mainnet-beta", "https://api.mainnet-beta.solana.com"],
 ]);
 
 export { rpc };
